fe59f7312e77a384a946a47b6ba70924b2cc3357
Matthew Archer <matt.james.archer@gmail.com>
2014-03-11 00:16:29 +0000

    Initial tutorials

---
+++ Window/Window.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include GLEW. Always include it before gl.h and glfw.h, since it's a bit magic.
+#include <GL/glew.h>
+
+// Include GLFW
+// #include <GL/glfw3.h>
+
+// Include GLM
+// #include <glm/glm.hpp>
+using namespace glm;
+
+int main(){
+
+// Initialise GLFW
+if( !glfwInit() )
+{
+    fprintf( stderr, "Failed to initialize GLFW\n" );
+        return -1;
+}
+
+glfwWindowHint(GLFW_FSAA_SAMPLES, 4); // 4x antialiasing
+glfwWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3); // We want OpenGL 3.3
+glfwWindowHint(GLFW_OPENGL_VERSION_MINOR, 3);
+glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL
+ 
+//
+// Open a window and create its OpenGL context
+GLFWwindow* window; // (In the accompanying source code, this variable is global)
+window = glfwCreateWindow( 1024, 768, "Tutorial 01", NULL, NULL);
+if( window == NULL ){
+    fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+    glfwTerminate();
+    return -1;
+}
+glfwMakeContextCurrent(window);
+ 
+// Initialize GLEW
+glewExperimental=true; // Needed in core profile
+if (glewInit() != GLEW_OK) {
+    fprintf(stderr, "Failed to initialize GLEW\n");
+    return -1;
+}
+
+}
